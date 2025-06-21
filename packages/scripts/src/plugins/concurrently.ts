60f23114c8b7ef9fcc445e778cd018ba7d8f4f84
Islam Yamor <islamyamorz@gmail.com>
2025-06-21 15:36:32 +0300

    fixed concurrently, json-server and tsx script plugins

--- packages/scripts/src/plugins/concurrently.ts
+++ packages/scripts/src/plugins/concurrently.ts
@@ -12,20 +12,25 @@ export class Concurrently implements Executable {
   prefixColors: string[]
   args: string[]
   packageExecutable = 'concurrently'
+
   constructor(opts: ConcurrentlyOpts) {
     this.names = opts.names
     this.prefixColors = opts.prefixColors
     this.args = opts.args
   }
+
   get command() {
     return [this.packageExecutable, this.namesArg, this.prefixColorsArg, this.argsFields].join(' ')
   }
+
   get namesArg() {
-    return `--names ${this.names}`
+    return `--names ${this.names.join(',')}`
   }
+
   get prefixColorsArg() {
-    return `--prefix-colors ${this.prefixColors}`
+    return this.prefixColors.length > 0 ? `--prefix-colors ${this.prefixColors.join(',')}` : ''
   }
+
   get argsFields(): string {
     return this.args.map((arg) => `"${arg}"`).join(' ')
   }
