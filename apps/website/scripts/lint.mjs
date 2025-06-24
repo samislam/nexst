4cc8f608a363a6d1b2693d39eaa39e5c7dda9d6a
Islam Yamor <islamyamorz@gmail.com>
2025-06-24 20:07:19 +0300

    enhanced linting for web and website and added type checking phase

--- apps/website/scripts/lint.mjs
+++ apps/website/scripts/lint.mjs
@@ -1,3 +1,22 @@
-import { Next, runCommandsSequentially } from '@repo/scripts'
+import { Echo, Next, runCommandsSequentially, Tspc } from '@repo/scripts'
+import chalk from 'chalk'
 
-runCommandsSequentially([new Next({ mode: 'lint' }).command])
+runCommandsSequentially(
+  [
+    new Echo(chalk.bold.cyanBright.italic('~ Type checking your project...')).command,
+    // type checking ---------
+    new Tspc({
+      noEmit: true,
+      tsconfigPath: './tsconfig.json',
+    }).command,
+    new Echo(chalk.greenBright('✔ No type errors were found')).command,
+    new Echo(chalk.bold.cyanBright.italic('~ Eslint is now checking your project...')).command,
+    // type checking ---------
+    new Next({
+      mode: 'lint',
+    }).command,
+    new Echo(chalk.bold.cyanBright.italic('~ You should be able to build your project now'))
+      .command,
+  ],
+  false
+)
