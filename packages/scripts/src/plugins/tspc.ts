4bb8ea5efd0d23d0982c18ce12acaf7d682c4920
Islam Yamor <islamyamorz@gmail.com>
2025-06-24 20:02:14 +0300

    added noEmit option to tspc script plugin

--- packages/scripts/src/plugins/tspc.ts
+++ packages/scripts/src/plugins/tspc.ts
@@ -3,22 +3,32 @@ import { Executable } from '@/types/executable-class'
 export interface TspcOpts {
   tsconfigPath?: string
   watch?: boolean
+  noEmit?: boolean
 }
+
+/** A script plugin for the `ts-patch` package */
 export class Tspc implements Executable {
   packageExecutable = 'tspc'
   tsconfigPath?: string
   watch?: boolean
+  noEmit?: boolean
   constructor(opts: TspcOpts = {}) {
     this.tsconfigPath = opts.tsconfigPath
     this.watch = opts.watch
+    this.noEmit = opts.noEmit
   }
   get tsconfigPathArg() {
-    return `-p ${this.tsconfigPath}`
+    return this.tsconfigPath ? `-p ${this.tsconfigPath}` : ''
   }
   get watchArg() {
     return this.watch ? `--watch` : ''
   }
+  get noEmitArg() {
+    return this.noEmit ? `--noEmit` : ''
+  }
   get command() {
-    return [this.packageExecutable, this.tsconfigPathArg, this.watchArg].join(' ')
+    return [this.packageExecutable, this.tsconfigPathArg, this.watchArg, this.noEmitArg]
+      .filter(Boolean)
+      .join(' ')
   }
 }
