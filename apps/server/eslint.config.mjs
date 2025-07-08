21cf8ed6e1a37e413e2da97e4c77f56207ec1460
Islam Yamor <islamyamorz@gmail.com>
2025-07-08 13:30:07 +0300

    fixed typescript configuration for nest project

--- apps/server/eslint.config.mjs
+++ apps/server/eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', 'src/generated/**'],
+    ignores: ['dist/**', 'src/generated'],
   },
   eslint.configs.recommended,
   tseslint.configs.recommended,
