a63eb4229ce14e580206abf983ce42d8f8912417
Islam Yamor <islamyamorz@gmail.com>
2025-07-08 13:14:37 +0300

    consumed LocaleTransition

--- apps/website/src/hooks/use-locale-transition.ts
+++ apps/website/src/hooks/use-locale-transition.ts
@@ -1,28 +1,4 @@
-import ms from 'ms'
-import { create } from 'zustand'
-import { AppLanguages } from '@/types/app-config'
+import { AppLanguages } from '@/types/app.types'
+import { createLocaleTransitionStore } from '@repo/react-utils'
 
-export interface LocaleTransitioningStore {
-  start: (pendingLanguage: AppLanguages) => void
-  stop: (opts?: { disableRTLSwitcher?: boolean; loadingDurationMs?: number }) => void
-  pendingLanguage?: AppLanguages
-  isChangingLocale: boolean
-}
-
-export const useLocaleTransition = create<LocaleTransitioningStore>((set, get) => ({
-  start(pendingLanguage) {
-    set({ isChangingLocale: true, pendingLanguage })
-  },
-  stop(opts = {}) {
-    const { disableRTLSwitcher, loadingDurationMs = ms('3s') } = opts
-    if (!disableRTLSwitcher) {
-      const state = get()
-      document.documentElement.setAttribute('dir', state.pendingLanguage === 'ar' ? 'rtl' : 'ltr')
-    }
-    setTimeout(() => {
-      set({ isChangingLocale: false })
-    }, loadingDurationMs)
-  },
-
-  isChangingLocale: false,
-}))
+export const useLocaleTransition = createLocaleTransitionStore<AppLanguages>()
