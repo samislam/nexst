40f994d15fc8045ebde203711b8db269126ecc69
Islam Yamor <islamyamorz@gmail.com>
2025-07-11 19:35:17 +0300

    formatted the code

--- apps/web/src/app/client-plugger.tsx
+++ apps/web/src/app/client-plugger.tsx
@@ -3,10 +3,13 @@
 import { useEffect } from 'react'
 import { useScroll } from '@repo/react-utils'
 
-/** Scrolls to the section defined in the current URL hash on initial page load. Does not attach any */
 export const ClientPlugger = () => {
   const [, scroll] = useScroll()
 
+  /**
+   * Scrolls to the section defined in the current URL hash on initial page load. Does not attach
+   * any
+   */
   useEffect(() => {
     const hash = window.location.hash
     if (hash) scroll(hash)
