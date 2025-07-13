c9b6bd07e39d00dcff092c306d61a10c4c4eaa17
Islam Yamor <islamyamorz@gmail.com>
2025-07-13 09:23:17 +0300

    organized middleware file and corrected usage for next-intl middleware and moved the comment to the top

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,12 +2,21 @@ import createMiddleware from 'next-intl/middleware'
 import { middlewareStack } from 'nextjs-middleware-stack'
 import { appRoutingDef } from './lib/next-intl/app-routing-def'
 
+const regExp = {
+  i18nExclude: /^(?!.*\/(api|_next\/static|_next\/image|favicon\.ico)).*$/,
+  // Always run for API routes
+  middlewareInclude: /\/(api|trpc)(.*)/,
+  // Skip Next.js internals and all static files, unless found in search params
+  middlewareExclude:
+    /\/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)/,
+}
+
 export default middlewareStack([
-  ['*', createMiddleware(appRoutingDef)],
   // ... your other middlewares
+  [regExp.i18nExclude, createMiddleware(appRoutingDef)],
 ])
 
 export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: [regExp.middlewareExclude, regExp.middlewareInclude],
+  runtime: 'nodejs',
 }
