c5a4e256db841e9d484e7efbf49235d0a92091e7
Islam Yamor <islamyamorz@gmail.com>
2025-08-02 17:16:41 +0300

    removed prisma section from package.json as it is now deprectated to be there

--- apps/web/prisma.config.ts
+++ apps/web/prisma.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   schema: './src/database/schema.prisma',
+  migrations: {
+    seed: 'tsx ./src/database/seed.ts',
+  },
 })
