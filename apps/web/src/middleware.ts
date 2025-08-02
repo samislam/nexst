8097d44f8d920001013e824acae623e8ff74850b
Islam Yamor <islamyamorz@gmail.com>
2025-08-02 18:01:15 +0300

    made next middleware matcher a raw string, to avoid next warning. and simplified the regExps for it.

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,11 +4,6 @@ import { appRoutingDef } from './lib/next-intl/app-routing-def'
 
 const regExp = {
   i18nExclude: /^(?!.*\/(api|_next\/static|_next\/image|favicon\.ico)).*$/,
-  // Always run for API routes
-  middlewareInclude: /\/(api|trpc)(.*)/,
-  // Skip Next.js internals and all static files, unless found in search params
-  middlewareExclude:
-    /\/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)/,
 }
 
 export default middlewareStack([
@@ -17,6 +12,9 @@ export default middlewareStack([
 ])
 
 export const config = {
-  matcher: [regExp.middlewareExclude, regExp.middlewareInclude],
+  matcher: [
+    '/(api|trpc)(.*)', // Always run for API routes,
+    '/((?!_next|.*\\..*).*)', // Skip Next.js internals and all static files, unless found in search params
+  ],
   runtime: 'nodejs',
 }
