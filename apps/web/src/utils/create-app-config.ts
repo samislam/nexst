a7d87baf856f08700133c28351d6576e849778f5
Islam Yamor <islamyamorz@gmail.com>
2025-08-04 07:54:47 +0300

    removed unused variables

--- apps/web/src/utils/create-app-config.ts
+++ apps/web/src/utils/create-app-config.ts
@@ -1,5 +1,3 @@
-import ms from 'ms'
 import { AppConfig } from '@/types/app.types'
-import { LOCALE_COOKIE_NAME } from '@/constants'
 
 export const createAppConfig = <L extends string>(ac: AppConfig<L>) => ac
