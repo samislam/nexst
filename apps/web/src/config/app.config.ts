0ac752e417c498771688a3b0c02600185a2a22b4
Islam Yamor <islamyamorz@gmail.com>
2025-08-10 10:01:23 +0300

    added new constants and renamed LOCALE_COOKIE constant

--- apps/web/src/config/app.config.ts
+++ apps/web/src/config/app.config.ts
@@ -1,7 +1,7 @@
 import ms from 'ms'
 import { concat } from 'concat-str'
 import logo from '@/media/logo.png'
-import { LOCALE_COOKIE_NAME } from '@/constants'
+import { LOCALE_COOKIE } from '@/constants'
 import { createAppConfig } from '@/utils/create-app-config'
 
 export default createAppConfig({
@@ -15,7 +15,7 @@ export default createAppConfig({
   i18nRoutingDef: {
     localePrefix: 'always', // defaults to have no URL prefix (no /en/users, just /users)
     localeCookie: {
-      name: LOCALE_COOKIE_NAME,
+      name: LOCALE_COOKIE,
       secure: false,
       sameSite: 'lax',
       maxAge: ms('1y'),
